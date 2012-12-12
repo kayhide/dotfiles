291aeaf84356f07ed415999aed495e7c30cbe3ff
kayhide <kayhide@gmail.com>
2012-12-12 13:51:52 +0900

    updated setup script.

---
+++ setup.rb
@@ -0,0 +1,23 @@
+$>.sync = true
+
+puts "updating git submodules."
+puts `git submodule update --init --recursive`
+
+exclusive_files = %w(. .. .git .gitignore .gitmodules)
+Dir["#{Dir.pwd}/.*"].reject do |f|
+  exclusive_files.include? File.basename(f)
+end.sort.each do |f|
+  puts "making symbolic link: #{f}"
+  puts `ln -sf #{f} ~/`
+end
+
+
+exclusive_putterns = %w(rinari/util/inf-ruby rinari/util/test/ert)
+ptn = /#{exclusive_putterns.join('|')}/
+el_files = Dir[".emacs.d/**/*.el"].reject do |f|
+  f =~ ptn
+end.sort
+puts "compiling elisp files: (#{el_files.count})"
+puts `emacs --batch -Q -l .emacs.d/init.el -f batch-byte-compile #{el_files.join ' '}`
+
+puts `rm wget-log*`
--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-git submodule update --init --recursive
-ln -sf $(pwd)/.* ~/
-rm ~/.git(@)
-rm ~/.gitignore(@)
-emacs --batch -Q -l .emacs.d/init.el -L .emacs.d/auto-install/ -f batch-byte-compile .emacs.d/**/*.el
