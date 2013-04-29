2a2d2803eeae9a7e1cc2890d6827e9ccf0f6e53d
kayhide <kayhide@gmail.com>
2013-04-30 03:22:42 +0900

    updated emacs setting.

--- .emacs.d/elisp/init-apple.el
+++ .emacs.d/elisp/init-apple.el
@@ -1,8 +1,28 @@
+(if window-system (set-frame-parameter nil 'alpha 85))
+
+(let* ((size 12)
+       (asciifont "Menlo")
+       (jpfont "Hiragino Maru Gothic ProN")
+       (h (* size 10))
+       (fontspec)
+       (jp-fontspec))
+  (set-face-attribute 'default nil :family asciifont :height h)
+  (setq fontspec (font-spec :family asciifont))
+  (setq jp-fontspec (font-spec :family jpfont))
+  (set-fontset-font nil 'japanese-jisx0208 jp-fontspec)
+  (set-fontset-font nil 'japanese-jisx0212 jp-fontspec)
+  (set-fontset-font nil 'japanese-jisx0213-1 jp-fontspec)
+  (set-fontset-font nil 'japanese-jisx0213-2 jp-fontspec)
+  (set-fontset-font nil '(#x0080 . #x024F) fontspec)
+  (set-fontset-font nil '(#x0370 . #x03FF) fontspec))
+
+
 ;; ------------------------------------------------------------------------
 ;; @ color-theme
 (add-to-list 'load-path "~/.emacs.d/elisp/color-theme")
 (require 'color-theme)
 (color-theme-initialize)
+
 
 ;; ------------------------------------------------------------------------
 ;; @ migemo
