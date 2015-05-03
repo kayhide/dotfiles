394e74a5b457820c0c932b34e51156923a3e542a
kayhide <kayhide@gmail.com>
2015-05-02 22:37:13 -0300

    update emacs settings.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,3 +5,17 @@
 
 (require 'init-loader)
 (init-loader-load)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((encoding . utf-8))))
+ '(yas-prompt-functions (quote (helm-yas-prompt))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erm-syn-errline ((t (:underline (:color "red")))))
+ '(erm-syn-warnline ((t (:underline (:color "orange"))))))
