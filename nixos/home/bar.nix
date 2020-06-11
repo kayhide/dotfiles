4ab6f93b54c8ff20d556158a2813ffff2da52e6c
Hideaki Kawai <kayhide@gmail.com>
2020-06-11 22:24:33 +0900

    Update polybar settings

--- nixos/home/bar.nix
+++ nixos/home/bar.nix
@@ -18,12 +18,18 @@ let
     white = "#e5e9f0";
     gray = "#4c566a";
   };
+  base-background = with_alpha "77" colors.black;
 
   with_alpha = alpha: color: with builtins;
     "#${alpha}${replaceStrings ["#"] [""] color}";
 
+  with_background = color: s:
+    "%{B${color}}${s}%{B-}";
+
+
   px-2 = s: "  ${s}  ";
-  i = s: "%{T2}${s}%{T-}";
+  sm = s: "%{T2}${s}%{T-}";
+  lg = s: "%{T3}${s}%{T-}";
 
   bar = {
     monitor = "\${env:MONITOR}";
@@ -37,9 +43,11 @@ let
 
     background = "#000f0f0f";
     foreground = colors.white;
-    font-0 = "NotoSans-Regular:size=10;2";
-    font-1 = "Material Icons:style=Regular:size=10;3";
-    # font-2 = "Font Awesome 5 Free,Font Awesome 5 Free Regular:style=Regular:size=10";
+    font-0 = "NotoSans Nerd Font:style=Regular:size=10;2";
+    font-1 = "NotoSans Nerd Font:style=Regular:size=8;3";
+    font-2 = "NotoSans Nerd Font:style=Regular:size=12;3";
+    font-3 = "Material Icons:style=Regular:size=12;4";
+    # font-4 = "Font Awesome 5 Free,Font Awesome 5 Free Regular:style=Regular:size=10";
   };
 in
 
@@ -56,12 +64,7 @@ in
       config = {
         "bar/base" = bar // {
           modules-left = "workspaces";
-          modules-right = "wireless-network wired-network pulseaudio backlight battery date";
-        };
-
-        "bar/sub1" = bar // {
-          modules-left = "workspaces";
-          modules-right = "wireless-network wired-network pulseaudio backlight battery date";
+          modules-right = "wireless-network wired-network pulseaudio backlight battery";
         };
 
         "module/date" = {
@@ -79,8 +82,10 @@ in
 
           label = px-2 "%percentage%%";
           format-background = with_alpha "77" colors.black;
-          format-prefix = px-2 (i "");
-          format-prefix-background = with_alpha "aa" colors.black;
+          format-prefix = px-2 (lg "");
+          format-prefix-background = with_alpha "99" colors.yellow;
+
+          # 
         };
 
         "module/battery" = {
@@ -89,21 +94,17 @@ in
           battery = "BAT0";
           adapter = "AC";
 
-          label-charging = px-2 "%percentage%%";
-          label-discharging = px-2 "%percentage%%";
-          label-full = px-2 "%percentage%%";
+          format-discharging = "${with_background (with_alpha "aa" colors.red) (px-2 "<ramp-capacity>")}${with_background base-background (px-2 "<label-discharging>")}";
+          format-charging = "${with_background (with_alpha "aa" colors.magenta) (px-2 "<ramp-capacity>${sm ""}")}${with_background base-background (px-2 "<label-charging>")}";
+          format-full = "${with_background (with_alpha "99" colors.black) (px-2 (lg ""))}${with_background base-background (px-2 "<label-full>")}";
 
-          format-charging-background = with_alpha "77" colors.black;
-          format-discharging-background = with_alpha "77" colors.black;
-          format-full-background = with_alpha "77" colors.black;
+          ramp-capacity-0 = lg "";
+          ramp-capacity-1 = lg "";
+          ramp-capacity-2 = lg "";
+          ramp-capacity-3 = lg "";
+          ramp-capacity-4 = lg "";
 
-          format-charging-prefix = px-2 (i "");
-          format-discharging-prefix = px-2 (i "");
-          format-full-prefix = px-2 (i "");
-
-          format-charging-prefix-background = with_alpha "aa" colors.yellow;
-          format-discharging-prefix-background = with_alpha "aa" colors.red;
-          format-full-prefix-background = with_alpha "aa" colors.cyan;
+          #       
         };
 
         "module/pulseaudio" = {
@@ -116,35 +117,41 @@ in
           format-volume-background = with_alpha "77" colors.black;
           format-muted-background = with_alpha "77" colors.black;
 
-          format-volume-prefix = px-2 (i "");
-          format-muted-prefix = px-2 (i "");
+          format-volume-prefix = px-2 (lg "");
+          format-muted-prefix = px-2 (lg "");
 
           format-volume-prefix-background = with_alpha "aa" colors.blue;
           format-muted-prefix-background = with_alpha "aa" colors.magenta;
+
+          #  
         };
 
         "module/wired-network" = {
           type = "internal/network";
           interface = "ens20u1u4";
 
-          label-connected = px-2 (i "");
+          label-connected = px-2 (lg "");
           format-connected-background = with_alpha "99" colors.green;
 
-          label-disconnected = px-2 (i "");
+          label-disconnected = px-2 (lg "");
           format-disconnected-background = with_alpha "cc" colors.red;
+
+          #  
         };
 
         "module/wireless-network" = {
           type = "internal/network";
           interface = "wlp59s0";
 
-          format-connected-prefix = px-2 (i "");
+          format-connected-prefix = px-2 (lg "");
           format-connected-prefix-background = with_alpha "cc" colors.black;
           label-connected = px-2 "%essid%";
           format-connected-background = with_alpha "77" colors.black;
 
-          label-disconnected = px-2 (i "");
+          label-disconnected = px-2 (lg "");
           format-disconnected-background = with_alpha "cc" colors.red;
+
+          #  
         };
 
         "module/workspaces" = {
