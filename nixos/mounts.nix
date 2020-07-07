b14af3de6dd6f78b94ec7585a4c426d51b34b682
Hideaki Kawai <kayhide@gmail.com>
2020-07-07 11:13:19 +0900

    Disable mounts config

--- nixos/mounts.nix
+++ nixos/mounts.nix
@@ -2,9 +2,9 @@
 
 {
   config = {
-    fileSystems."/mnt/hdlan16" = {
-      device = "//192.168.48.27/pub";
-      fsType = "cifs";
-    };
+    # fileSystems."/mnt/hdlan16" = {
+    #   device = "//192.168.48.27/pub";
+    #   fsType = "cifs";
+    # };
   };
 }
