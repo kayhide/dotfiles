42c9008482f6e8d34645e3e71198fcf5cca3e35c
Hideaki Kawai <kayhide@gmail.com>
2021-05-26 21:49:35 +0900

    Update i3 autostart script

--- nixos/dotfiles/i3/autostart.sh
+++ nixos/dotfiles/i3/autostart.sh
@@ -1,20 +1,4 @@
-set-monitor() {
-    local monitor="$1"
-    local mode="$2"
-    local pos="${3:-0x0}"
-    xrandr --output $monitor --mode $mode --pos $pos
-}
-
-# set-monitor eDP-1 2560x1440
-# set-monitor DP-3 1920x1080 2560x0
-xrandr --listmonitors |tail -n +2 |while read -r line; do
-    name="${line##* }"
-    if [[ $line =~ "*" ]]; then
-        set-monitor "$name" 2560x1440
-    elif [[ $name =~ 3 ]]; then
-        set-monitor "$name" 1920x1080 2560x0
-    fi
-done
+set-monitor eDP-1 2560x1440
 
 ergo &
 keynav &
@@ -23,17 +7,10 @@ parcellite &
 pasystray &
 dropbox &
 
-if [[ -n ${SAVE_LAST_PWD+x} && -f $SAVE_LAST_PWD ]]; then
-    rm $SAVE_LAST_PWD
-fi
-
 # Monitor relatetd settings
 # Waiting 5 seconds is good enough for monitors to get ready.
 sleep 5
 
-for i in $(xrandr --listmonitors |grep "[0-9]\+:" |cut -d : -f 1); do
-    ~/bin/conky-clock "$i" &
-done
-
+systemctl --user restart conky-clock &
 systemctl --user restart wallpaper &
 systemctl --user restart polybar &
