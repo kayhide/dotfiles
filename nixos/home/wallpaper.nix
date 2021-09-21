3decefbbcd25d422f2d5e8c149867a669c986168
Hideaki Kawai <kayhide@gmail.com>
2021-09-21 23:29:19 +0900

    Update wallpaper to xwinwrap when switching images

--- nixos/home/wallpaper.nix
+++ nixos/home/wallpaper.nix
@@ -3,6 +3,7 @@
 let
   bin-path = with pkgs; lib.makeBinPath [
     coreutils
+    feh
     gnused
     xorg.xrandr
     xwinwrap
@@ -11,13 +12,22 @@ let
   wallpaper = with pkgs; writeScriptBin "wallpaper" ''
     #!${stdenv.shell}
 
-    PATH=${xscreensaver}/libexec/xscreensaver:${bin-path}:$PATH
+    PATH=${bin-path}:$PATH
+    wallpaper_dir="$HOME/.wallpaper"
 
-    xrandr --listmonitors | cut -d " " -f 4 | sed -n "s=/[0-9]*==gp" | while read -r g; do
-      xwinwrap -g "$g" -ov -- glslideshow -window-id WID -zoom 100 -clip -duration 900 -pan 900 &
+    width=0
+    height=0
+    for g in $(xrandr --listmonitors | cut -d " " -f 4 | sed -n "s=/[0-9]*==gp"); do
+      if [[ $g =~ ([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+) ]]; then
+        w=$(( ''${BASH_REMATCH[1]} + ''${BASH_REMATCH[3]} ))
+        h=$(( ''${BASH_REMATCH[2]} + ''${BASH_REMATCH[4]} ))
+        (( width < w )) && width=$w
+        (( height < h )) && height=$h
+      fi
     done
-
-    sleep 1
+    if (( 0 < width && 0 < height )); then
+      xwinwrap -g "''${width}x''${height}+0+0" -ov -- feh --no-fehbg --bg-center --bg-fill --randomize --recursive "$wallpaper_dir"
+    fi
   '';
 
 in
@@ -29,17 +39,33 @@ in
         wallpaper = {
           Unit = {
             Description = "Wallpaper";
-            After = [ "graphical-session-pre.target" "conky-clock.service" ];
+            After = [ "graphical-session-pre.target" ];
             PartOf = [ "graphical-session.target" ];
           };
           Service = {
             Type = "oneshot";
             ExecStart = "${wallpaper}/bin/wallpaper";
             IOSchedulingClass = "idle";
-            RemainAfterExit = true;
           };
           Install = {
             WantedBy = [ "graphical-session.target" ];
+          };
+        };
+      };
+
+      timers = {
+        wallpaper = {
+          Unit = {
+            Description = "Wallpaper timer";
+          };
+          Timer = {
+            Unit = "wallpaper.service";
+            OnCalendar = "*-*-* *:0/15:0";
+            Persistent = true;
+            RandomizedDelaySec = "1s";
+          };
+          Install = {
+            WantedBy = [ "timers.target" ];
           };
         };
       };
