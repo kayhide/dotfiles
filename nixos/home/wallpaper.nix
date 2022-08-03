c68710892f4df07133fbd2e7e343266cbf03b90d
Hideaki Kawai <kayhide@gmail.com>
2022-08-03 16:51:37 +0900

    Update wallpaper script

--- nixos/home/wallpaper.nix
+++ nixos/home/wallpaper.nix
@@ -4,6 +4,8 @@ let
   bin-path = with pkgs; lib.makeBinPath [
     coreutils
     feh
+    findutils
+    gnugrep
     gnused
     xorg.xrandr
     xwinwrap
@@ -12,22 +14,48 @@ let
   wallpaper = with pkgs; writeShellScriptBin "wallpaper" ''
     PATH=${bin-path}:$PATH
     wallpaper_dir="$HOME/.wallpaper"
+    current="$HOME/.cache/wallpaper/current"
+
+    count="$(xrandr --listmonitors | head -n 1 | sed "s/^.*: *//")"
+
+    verify-current() {
+      if find "$current" -mmin -1 | grep .; then
+        lines="$(wc -l < "$current")"
+        if (( lines == count )); then
+            return 0
+        fi
+      fi
+      return 1
+    }
+
+    refresh-current() {
+      mkdir -p "$(dirname "$current")"
+      find -L "$wallpaper_dir" | shuf -n "$count" > "$current"
+    }
+
+    verify-current || refresh-current
+
 
     width=0
     height=0
     for g in $(xrandr --listmonitors | cut -d " " -f 4 | sed -n "s=/[0-9]*==gp"); do
       if [[ $g =~ ([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+) ]]; then
-        w=$(( ''${BASH_REMATCH[1]} + ''${BASH_REMATCH[3]} ))
-        h=$(( ''${BASH_REMATCH[2]} + ''${BASH_REMATCH[4]} ))
+        w=$(( BASH_REMATCH[1] + BASH_REMATCH[3] ))
+        h=$(( BASH_REMATCH[2] + BASH_REMATCH[4] ))
         (( width < w )) && width=$w
         (( height < h )) && height=$h
       fi
     done
     if (( 0 < width && 0 < height )); then
-      xwinwrap -g "''${width}x''${height}+0+0" -ov -- feh --no-fehbg --bg-center --bg-fill --randomize --recursive "$wallpaper_dir"
+      readarray -t files < "$current"
+      xwinwrap -g "''${width}x''${height}+0+0" \
+        -o 0.5 \
+        -ov \
+        -- \
+        feh --no-fehbg --bg-center --bg-fill "''${files[@]}"
     fi
 
-    # It needs some time before conky starts
+    # Give some time for programs waiting for wallpaper being updated
     sleep 1
   '';
 
