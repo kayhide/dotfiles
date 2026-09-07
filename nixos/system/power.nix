82ac7080c51bd167a37ef56e4ba1a1f89e228799
Hideaki Kawai <kayhide@gmail.com>
2026-09-08 08:36:39 +0900

    Update nixos power config

--- nixos/system/power.nix
+++ nixos/system/power.nix
@@ -4,6 +4,11 @@
   services.logind.settings = {
     Login = {
       HandlePowerKey = "lock";
+
+      # Keep the machine up with the lid closed so SSH stays reachable.
+      HandleLidSwitch = "ignore";
+      HandleLidSwitchDocked = "ignore";
+      HandleLidSwitchExternalPower = "ignore";
     };
   };
 }
